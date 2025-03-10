3970e006848b5c39c61ba895b9767b1e44da3134
Brian Belaj <belaj.br@gmail.com>
2025-03-10 10:42:23 +0100

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { generateSitemap } from 'vite-plugin-sitemap'
+import sitemapPlugin from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     createHtmlPlugin(),
-    generateSitemap({
+    sitemapPlugin({
       hostname: "https://brianbelaj.netlify.app", // Aggiungi qui tutte le tue pagine
     }),
   ],
