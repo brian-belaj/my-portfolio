9db2bc90bfe6a1b5e9b6f7603e6ccf027efcc1ba
Brian Belaj <belaj.br@gmail.com>
2025-03-14 17:54:30 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTelegram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -11,6 +11,9 @@ const Footer = () => {
           </a>
           <a href="https://www.linkedin.com/in/brian-belaj/" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-blue-500">
             <FaLinkedin />
+          </a>
+          <a href="https://t.me/brianbelaj" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-blue-400">
+            <FaTelegram />
           </a>
         </div>
         <p className="text-sm text-gray-400 mt-4">
